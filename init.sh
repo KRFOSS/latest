07a9103c1679b546727f620fe96c0e205ad3cb39
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2025-08-17 01:33:10 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,19 +4,19 @@ CODENAME=$(lsb_release -cs 2>/dev/null || grep -oP 'VERSION_CODENAME=\K\w+' /etc
 COMPONENTS="main contrib non-free non-free-firmware"
 
 cat > /etc/apt/sources.list << EOF
-deb https://http.krfoss.org/debian $CODENAME $COMPONENTS
-deb-src https://http.krfoss.org/debian $CODENAME $COMPONENTS
+deb https://${MIRROR_URL}/debian $CODENAME $COMPONENTS
+deb-src https://${MIRROR_URL}/debian $CODENAME $COMPONENTS
 
-deb https://http.krfoss.org/debian $CODENAME-updates $COMPONENTS
-deb-src https://http.krfoss.org/debian $CODENAME-updates $COMPONENTS
+deb https://${MIRROR_URL}/debian $CODENAME-updates $COMPONENTS
+deb-src https://${MIRROR_URL}/debian $CODENAME-updates $COMPONENTS
 
 # security updates
-deb https://http.krfoss.org/debian-security $CODENAME-security $COMPONENTS
-deb-src https://http.krfoss.org/debian-security $CODENAME-security $COMPONENTS
+deb https://${MIRROR_URL}/debian-security $CODENAME-security $COMPONENTS
+deb-src https://${MIRROR_URL}/debian-security $CODENAME-security $COMPONENTS
 
 # $CODENAME-backports, previously on backports.debian.org
-deb https://http.krfoss.org/debian $CODENAME-backports $COMPONENTS
-deb-src https://http.krfoss.org/debian $CODENAME-backports $COMPONENTS
+deb https://${MIRROR_URL}/debian $CODENAME-backports $COMPONENTS
+deb-src https://${MIRROR_URL}/debian $CODENAME-backports $COMPONENTS
 EOF
 
 apt update -y
